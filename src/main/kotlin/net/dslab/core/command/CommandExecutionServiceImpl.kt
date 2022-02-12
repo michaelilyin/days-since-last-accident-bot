3a5a23d7bf585d2b72a2598f85e4f99cbad6865b
Michael Ilyin <michael.s.ilyin@gmail.com>
2022-02-12 19:27:55 +0300

    initial commit

--- src/main/kotlin/net/dslab/core/command/CommandExecutionServiceImpl.kt
+++ src/main/kotlin/net/dslab/core/command/CommandExecutionServiceImpl.kt
@@ -7,7 +7,9 @@ import net.dslab.core.message.builder.MessageBuilder
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.inject.Instance
 import javax.inject.Inject
+import kotlin.reflect.KClass
 import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.superclasses
 
 @ApplicationScoped
 class CommandExecutionServiceImpl @Inject constructor(
@@ -16,14 +18,43 @@ class CommandExecutionServiceImpl @Inject constructor(
 ) : CommandExecutionService {
     private val chain: CommandChain = commands.asSequence()
         .sortedBy {
-            val klass = it::class
-            val priority = klass.findAnnotation<CommandPriority>()
-                ?: throw IllegalArgumentException("${klass.simpleName} must be annotated with @CommandPriority")
+            val priority = getPriority(it)
             -1 * (priority.phase.order + priority.factor)
         }
         .fold(LastCommandChain() as CommandChain) { acc, command ->
             UsualCommandChain(command, acc)
         }
+
+    private fun getPriority(command: Command): CommandPriority {
+        var klass = command::class
+        val priority = findForClass(klass)
+
+
+
+        if (priority == null) {
+            throw IllegalArgumentException("${klass.simpleName} must be annotated with @CommandPriority")
+        }
+        return priority
+    }
+
+    private fun findForClass(klass: KClass<*>): CommandPriority? {
+        val priority = klass.findAnnotation<CommandPriority>()
+
+        if (priority != null) {
+            return priority
+        }
+
+        val superclasses = klass.superclasses
+        for (superclass in superclasses) {
+            val superPriority = findForClass(superclass)
+
+            if (superPriority != null) {
+                return superPriority
+            }
+        }
+
+        return null
+    }
 
     override fun run(input: CommandExecutionContext, builder: MessageBuilder<*>) {
         logger.info { "Invoking command for $input" }
